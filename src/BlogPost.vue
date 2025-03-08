dd6c60f1122f6d5e97cc4a54e718517e9421ed5f
Robert-Hannaford <154273971+Robert-Hannaford@users.noreply.github.com>
2025-03-08 15:53:11 -0600

    pass data to child components with props

---
+++ src/BlogPost.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="blog-post">
+        <p>
+            {{ message }}
+        </p>
+        <!-- display id, title, and content of a blog post -->
+         <h2>{{ id }} - {{ blogPostTitle }}</h2>
+         <h4>{{ blogPostContent }}</h4>
+    </div>
+</template>
+
+<script setup>
+
+import { ref } from 'vue'
+let message = ref('This is the BlogPost component.')
+defineProps(['id', 'blogPostTitle', 'blogPostContent'])
+// defineProps({
+//     id: Number,
+//     blogPostTitle: String,
+//     blogPostContent: String
+// })
+</script>
+
+<style scoped>
+.blog-post{
+    background-color: aqua;
+    padding: 10px;
+    margin-bottom: 10px;
+}
+</style>
